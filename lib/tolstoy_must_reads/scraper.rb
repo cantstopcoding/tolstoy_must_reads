b68baa2bc30862bf0d98c81e7112eac40f1104de
cantstopcoding <jonathan_koss@yahoo.com>
2020-04-13 19:03:15 -0400

    get rid of unnecessary comments

--- lib/tolstoy_must_reads/scraper.rb
+++ lib/tolstoy_must_reads/scraper.rb
@@ -1,6 +1,6 @@
 
 class TolstoyMustReads::Scraper
-    def self.scrape_all # Does it need to be a class or instance method?
+    def self.scrape_all 
         page = "https://theculturetrip.com/europe/russia/articles/the-10-best-books-by-leo-tolstoy-you-have-to-read/"
         doc = Nokogiri::HTML(open(page))
         title_elements = doc.css("div.grid__Grid-u88k9r-0.article-grid__ArticleGrid-sc-5b0lbv-0.article-jsonstyled__ArticleV2ContentGrid-mn7kve-2.hquILT .fORVIn")
